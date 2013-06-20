37be8f9e12e3866aba4bd20c8d47ec30c783685f
Flemming Madsen <mail@leflings.dk>
2013-06-20 08:40:50 +0200

    Slowstop should work. General cleanup

--- src/dk/dtu/cdio/ANIMAL/brick/Communicator.java
+++ src/dk/dtu/cdio/ANIMAL/brick/Communicator.java
@@ -4,7 +4,6 @@ import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 
-import lejos.nxt.LCD;
 import lejos.nxt.Sound;
 import lejos.nxt.comm.BTConnection;
 import lejos.nxt.comm.Bluetooth;
--- src/dk/dtu/cdio/ANIMAL/brick/Unit.java
+++ src/dk/dtu/cdio/ANIMAL/brick/Unit.java
@@ -3,54 +3,33 @@ package dk.dtu.cdio.ANIMAL.brick;
 import java.io.IOException;
 
 import lejos.robotics.RegulatedMotor;
-import lejos.robotics.navigation.DifferentialPilot;
 import lejos.util.PilotProps;
 
 public class Unit {
 
-	protected DifferentialPilot pilot;
 	private Communicator com;
-//	public int travelSpeed = 0;
 	
 	Command nextCommand;
 	Command currentcommand;
 	
-	double travelSpeed = 0, steerRatio;
+	double steerRatio;
 	RegulatedMotor left, right, inside, outside;
 	int motorSpeed;
 
 	public Unit() {
 		left = PilotProps.getMotor("A");
 		right = PilotProps.getMotor("B");
+		
 		motorSpeed = (int) (0.8f * left.getMaxSpeed());
-		left.setSpeed(motorSpeed);
-		right.setSpeed(motorSpeed);
-//		this.pilot = new DifferentialPilot(43.3, 10.15, left, right, false);
+		execute(new Command(NavCommand.SET_TRAVELSPEED, motorSpeed, 0, 0, false));
+		
 		com = new Communicator(this);
+		
 		System.out.println("LEFT: " + left.getMaxSpeed());
 		System.out.println("RIGHT: " + right.getMaxSpeed());
 	}
 
 	public static void main(String[] args) throws IOException, InterruptedException {
-		PilotProps pp = new PilotProps();
-//		pp.loadPersistentValues();
-//		if(!Boolean.parseBoolean(pp.getProperty("CDIO_DEF"))) {
-//			System.out.println("prop not set");
-//			pp.setProperty(PilotProps.KEY_WHEELDIAMETER, "56");
-//			pp.setProperty( PilotProps.KEY_TRACKWIDTH, "120");
-//			pp.setProperty( PilotProps.KEY_LEFTMOTOR, "B");
-//			pp.setProperty( PilotProps.KEY_RIGHTMOTOR, "C");
-//			pp.setProperty( PilotProps.KEY_REVERSE, "false");
-//			pp.setProperty("CDIO_DEF", "true");
-//			pp.storePersistentValues();
-//		}
-//		float wheelDiameter = Float.parseFloat(pp.getProperty( PilotProps.KEY_WHEELDIAMETER, "56"));
-//		float trackWidth = Float.parseFloat(pp.getProperty( PilotProps.KEY_TRACKWIDTH, "120"));
-//		RegulatedMotor leftMotor = PilotProps.getMotor(pp.getProperty( PilotProps.KEY_LEFTMOTOR, "B"));
-//		RegulatedMotor rightMotor = PilotProps.getMotor(pp.getProperty( PilotProps.KEY_RIGHTMOTOR, "C"));
-//		boolean reverse = Boolean.parseBoolean(pp.getProperty( PilotProps.KEY_REVERSE, "false"));
-
-
 		Unit unit = new Unit();
 		unit.go();
 	}
